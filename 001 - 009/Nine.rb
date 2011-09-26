e7307d699264570842b73ec5ccb9a7363b2b0ef0
David Mah <Mahhaha@gmail.com>
2011-09-26 01:27:04 -0700

    Light Refactor on Eight and Nine
    - Added Documentation for problems
    - Made syntax more ruby-like but did not change algorithms

--- 001 - 009/Nine.rb
+++ 001 - 009/Nine.rb
@@ -1,10 +1,23 @@
-a = 1
-b = 1
-c = 1
-for i in 1..998
-	for j in 1..(1000 - i)
-		k = 1000 - i - j
-			result = i * j * k if i * i + j * j == k * k
-	end
+# Nine.rb
+#
+# A Pythagorean triplet is a set of three natural numbers, a  b  c, for which,
+#
+# a2 + b2 = c2
+# For example, 32 + 42 = 9 + 16 = 25 = 52.
+#
+# There exists exactly one Pythagorean triplet for which a + b + c = 1000.
+# Find the product abc.
+#
+# Process:
+# Giant Brute Force
+# Try all combinations of a, b, c that add to 1000
+# O(n^3)
+
+result = -1
+(1..998).each do |a|
+  (1..(1000 - a)).each do |b|
+    c = 1000 - a - b
+    result = a * b * c if a**2 + b**2 == c**2
+  end
 end
 puts result
